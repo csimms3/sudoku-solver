7551b606c4a1ebe7a862160db7fcd54bdf7da4ca
csimms <cmsimms109@gmail.com>
2023-08-15 18:12:06 -0700

    add loadBoard function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@
 
 
 
-
 /**
  * Generates an empty sudoku board
  *  - each 3x3 section enumerated from 0 to 8
@@ -37,12 +36,28 @@ function generateBoard() {
 
 
 /**
- * Adds specified number (1-9) to specified square
+ * 
+ * @param {number} value 
+ * @param {number} cellPos
+ * 
+ * Inserts value into cell at positon cellPos
  */
-
-function insertValue(value, cellNum) {
-    const cell = document.querySelector("#cell" + cellNum);
+function insertValue(value, cellPos) {
+    const cell = document.querySelector("#cell" + cellPos);
     cell.textContent = value;
+}
+
+/**
+ * 
+ * @param {number[81]} boardList - sudoku board in array form
+ * 
+ * Loads the sudoku array into the displayed board
+ */
+function loadBoard(boardList) {
+    for (let i = 0; i<81; i++) {
+        const cell = document.querySelector("#cell" + i);
+            cell.textContent = boardList[i];
+    }
 }
 
 
@@ -57,12 +72,15 @@ function clearBoard() {
 
 
 
-
+// BEGIN FUNCTION CALLS
 
 generateBoard();
 
+board2 = []
 for (let i = 0; i<81; i++) {
-    insertValue("6", i);
+    board2.push(i%9+1);
 }
 
-clearBoard();
+
+loadBoard(board2);
+
--- js/sudoku.js
+++ js/sudoku.js
@@ -19,4 +19,25 @@ const board = [
         0, 0, 0,  0, 0, 0,  0, 0, 0,
 ]
 
-console.log(board[0]);
+const board2 = []
+
+for (let i = 0; i<81; i++) {
+    board2.push(i%9+1);
+}
+
+
+/**
+ * 
+ * @param {number[81]} sudoku board
+ * 
+ * returns True is board is solved, False otherwise
+ */
+function boardSolved(board) {
+    for (let i=0; i<81; i++) {
+        if (!(board[i] <= 9 && board[i] >= 1))
+            return false;
+    }
+    return true;
+}
+
+console.log(boardSolved(board2));
