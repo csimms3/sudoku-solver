222b2b5d3d6b02d856380ae30cff165707511c0a
csimms <cmsimms109@gmail.com>
2023-08-15 17:46:17 -0700

    add insert value functionality

--- js/script.js
+++ js/script.js
@@ -3,44 +3,59 @@
 
 /**
  * Generates an empty sudoku board
+ *  - each 3x3 section enumerated from 0 to 8
+ *  - cells enumerated from 0 to 80
  */
 function generateBoard() {
 
     const board = document.querySelector("#board");
     console.log(board);
-    for (let i = 0; i < 9; i++) {
-        const section = generateBoardSection();
+    for (let i=0; i < 9; i++) {
+        const section = document.createElement('div');
+        section.classList.add('section');
+        section.setAttribute('id', "section" + i);
+
+        //fill individual 3x3 section
+        for (let j=0; j < 9; j++) {
+            const cell = document.createElement('div');
+            cell.setAttribute('id', "cell" + (9*i + j));
+            cell.classList.add('cell');
+            section.appendChild(cell);
+        }
+        
         board.appendChild(section);
     }
 
 }
 
-/**
- * Generates a single 3x3 board section
- */
-function generateBoardSection() {
-    const section = document.createElement('div');
-    section.classList.add('section');
 
-    for (let i = 0; i < 9; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        section.appendChild(cell);
-    }
-    return section;
+/**
+ * Adds specified number (1-9) to specified square
+ */
+
+function insertValue(value, cellNum) {
+    const cell = document.querySelector("#cell" + cellNum);
+    cell.textContent = value;
 }
 
 
-
-
-
-
-
-
-
+/**
+ * Clears the current board
+ */
+function clearBoard() {
+    for (let i = 0; i<81; i++) {
+        insertValue("", i);
+    }
+}
 
 
 
 
 
 generateBoard();
+
+for (let i = 0; i<81; i++) {
+    insertValue("6", i);
+}
+
+clearBoard();
